b58564f94a84a81599051ee01267db44f32c9d33
Kristofer M White <me@kmwhite.net>
2012-12-08 00:27:59 +0000

    Initial add of Pelican blog

---
+++ settings.py
@@ -0,0 +1,30 @@
+AUTHOR = u'Kristofer M White'
+SITENAME = u'kmwhite.net'
+SITEURL = u'http://www.kmwhite.net'
+TIMEZONE = "America/Chicago"
+THEME = 'tuxlite_tbs'
+
+GITHUB_URL = 'http://github.com/kmwhite/'
+PDF_GENERATOR = False
+REVERSE_CATEGORY_ORDER = True
+LOCALE = "C"
+DEFAULT_PAGINATION = 4
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+LINKS = (('Techno-Geek', "http://www.techno-geeks.org/"),)
+
+SOCIAL = (('twitter', 'http://twitter.com/_kmwhite'),
+          ('github', 'http://github.com/kmwhite'),
+          ('bitbucket', 'http://bitbucket.org/kmwhite'))
+
+# global metadata to all the contents
+# DEFAULT_METADATA = (('yeah', 'it is'),)
+
+# static paths will be copied under the same name
+STATIC_PATHS = ["images", ]
+
+# A list of files to copy from the source to the destination
+FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),)
+
---
+++ src/00-first-post.md
@@ -0,0 +1,4 @@
+Date: 2012-12-10
+Title: Hello World
+
+## Hello World!
