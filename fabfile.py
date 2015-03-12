34fb00dc78c47d18b6e4b51ac0d6721881c3d3bc
Kristofer M White <me@kmwhite.net>
2015-03-11 19:46:46 -0500

    Migrating to fabric usage

---
+++ fabfile.py
@@ -0,0 +1,39 @@
+from fabric.api import local
+import os
+import shutil
+import sys
+import SimpleHTTPServer
+import SocketServer
+
+# Port for `serve`
+PORT = 8000
+
+def build():
+    """Build local version of site"""
+    # Generate the static content
+    # -v for verbose mode
+    # -D for debug output
+    # -s for specifying a custom settings file
+    local('pelican -s settings.py src/')
+
+def regenerate():
+    """Automatically regenerate site upon file modification"""
+    fabric.api.local('pelican -r -s pelicanconf.py')
+
+def serve():
+    """Serve site at http://localhost:8000/"""
+    os.chdir('output')
+
+    class AddressReuseTCPServer(SocketServer.TCPServer):
+        allow_reuse_address = True
+
+    server = AddressReuseTCPServer(('', PORT), SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+    sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
+    server.serve_forever()
+
+def publish():
+    """Publish to Heroku"""
+    build()
+    os.chdir('output')
+    local('git commit -a && git push')
